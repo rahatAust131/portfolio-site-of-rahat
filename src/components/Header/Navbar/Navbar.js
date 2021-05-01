90cf5b27eca3df2649299d43d147a79e08b3ecf4
Rahat Chowdhury <rczisan@gmail.com>
2021-05-01 21:35:52 +0600

    added navbar

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            This is Navbar
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="">{"<DevRahat>"}</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ms-auto">
+                        <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+                        <Link className="nav-link" to="/about">About</Link>
+                        <Link className="nav-link" to="/projects">Projects</Link>
+                        <Link className="nav-link" to="/blogs">Blogs</Link>
+                        <Link className="nav-link" to="/resume">Resume</Link>
+                        <Link className="nav-link" to="/contact">Contact</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header />
             <h2>This is Home </h2>
         </div>
     );
