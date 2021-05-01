70c6406685f486175ab91b427dd09f6ae4fdc825
Rahat Chowdhury <rczisan@gmail.com>
2021-05-01 20:54:30 +0600

    added routes

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h3>This is the ABOUT section</h3>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Header/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar />
+            <h2>This is Header</h2>
+        </div>
+    );
+};
+
+export default Header;
