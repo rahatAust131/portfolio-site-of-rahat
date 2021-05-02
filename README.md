57c22ef86ee731a4f49a072ff3d8ad604f904da9
Rahat Chowdhury <rczisan@gmail.com>
2021-05-02 21:44:33 +0600

    added readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio Website
+live link
+# https://portfolio-5fd71.web.app/
+This is my portfolio website created with react & bootstrap. Feel free to share your opinion & give your review
--- src/components/Header/Header/Header.js
+++ src/components/Header/Header/Header.js
@@ -9,10 +9,10 @@ const Header = () => {
                 <h2><span className="text-brand">Rahat</span> Chowdhury</h2>
                 <h3>WEB Developer | Designer</h3>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium nulla nemo expedita temporibus reiciendis voluptate vero provident, at excepturi debitis obcaecati non aperiam aliquam quasi porro iusto distinctio adipisci! Enim eligendi, nobis recusandae impedit explicabo quasi possimus suscipit qui quisquam, molestias illum eos, deleniti repellendus!</p>
-                <button className="btn btn-brand w-25">Hire Me</button>
+                <a href="https://drive.google.com/u/0/uc?id=17xb8QK7NORhnLaqdtu_ENnTma58pHjLV&export=download"><button className="btn btn-brand w-25">Hire Me</button></a>
             </div>
             <div className="col-md-6 header-columns header-second-column">
-                <img src={myImage} className="header-img img-fluid" alt=""/>
+                <img src={myImage} className="header-img img-fluid" alt="" />
             </div>
         </div>
     );
